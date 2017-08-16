fdcadcdb652d6e26f737b7e385a4100ccaadcb9d
Troels Fedder Jensen <tfj@ase.au.dk>
2017-08-16 15:11:08 +0200

    Added silly tets case

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -7,11 +7,18 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
+    public void ItemNameShouldBePreserved() {
         Item[] items = new Item[] { new Item("fixme", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    public void AgedBrieShouldIncreaseInValueAsTimePasses(){
+    	Item[] items = new Item[] { new Item("Aged brie", 10, 10) };
+	GildedRose uut = new GildedRose(items);
+	app.updateQuality();
+	assertEquals("Aged brie", uut.items[0].name);
+	}
 }
